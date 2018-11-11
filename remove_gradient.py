860d94626cca42dc93da52a5055a7c3842f750e0
NeuroDebian User <brain@neurodebian>
2018-11-10 20:08:26 -0500

    Add function for gradient subtraction

--- remove_gradient.py
+++ remove_gradient.py
@@ -9,7 +9,7 @@ from mne.preprocessing import ICA
 import matplotlib.pyplot as plt
 
 montage = mne.channels.read_montage('standard-10-5-cap385',path='/media/sf_E_DRIVE/')
-raw = mne.io.read_raw_brainvision('/media/sf_E_DRIVE/badger_eeg/russell/retino_gamma_01.vhdr',montage=montage,eog=[31])
+raw = mne.io.read_raw_brainvision('/media/sf_E_DRIVE/badger_eeg/russell/retino_allstim_02.vhdr',montage=montage,eog=[31])
 
 graddata = raw.get_data()[0:64,:]
 
@@ -34,7 +34,8 @@ def get_slicepochs(graddata, slicegap):
         slice_epochs[:,icount,:] = graddata[:,i:i+slicegap]
         slice_inds[icount,:] = np.arange(i,i+slicegap) 
         icount = icount + 1 
-    
+            
+    slice_inds = slice_inds.astype(int)
     return slice_epochs, slice_inds
 
 def remove_nongradient_epochs(slice_epochs, channel=3, corrthresh=0.9):
@@ -51,30 +52,45 @@ def remove_nongradient_epochs(slice_epochs, channel=3, corrthresh=0.9):
         min_index = grad_epochs[np.argmin(dists_i)]
         slice_epochs[:,i,:] = slice_epochs[:,min_index,:]
 
-    return slice_epochs
+    return slice_epochs, mean_corrmat > corrthresh
+
+def subtract_gradient(slice_epochs, slice_inds, corrmat_thresh, window_sz=60, window_std=5):
+    window_sz = 60
+    window_std = 5
+    window = signal.gaussian(window_sz, std=window_std)
+    window = np.reshape(window / np.sum(window),[window.shape[0],1])
+    
+    smooth_epochs = np.zeros(slice_epochs.shape)
+    for i in np.arange(0,slice_epochs.shape[0]):
+        chan_i = np.squeeze(slice_epochs[i,:,:])
+        smooth_epochs[i,:,:] = signal.convolve2d(chan_i[:,:], window, boundary='wrap', mode='same')
+        print(i)
+    
+    subbed_epochs = slice_epochs - smooth_epochs
+    subbed_ts = np.zeros(graddata.shape)
+    for i in np.arange(0,slice_epochs.shape[1]):
+        if corrmat_thresh[i] == 1:
+            subbed_ts[:,slice_inds[i,:]] = subbed_epochs[:,i,:]
+        else:
+            subbed_ts[:,slice_inds[i,:]] = 0
+            
+    return subbed_ts
+
 
 slicegap = get_slicegap(graddata)
 slice_epochs, slice_inds = get_slicepochs(graddata, slicegap)
-slice_epochs = remove_nongradient_epochs(slice_epochs)
-
-# smooth and subtract
-window_sz = 51
-window_std = 7
-window = signal.gaussian(window_sz, std=window_std)
-window = np.reshape(window / np.sum(window),[window.shape[0],1])
+slice_epochs, corrmat_thresh = remove_nongradient_epochs(slice_epochs)
+subbed_ts = subtract_gradient(slice_epochs, slice_inds, corrmat_thresh)
 
 
-smooth_epochs = np.zeros(slice_epochs.shape)
-for i in np.arange(0,slice_epochs.shape[0]):
-    chan_i = np.squeeze(slice_epochs[i,:,:])
-    smooth_epochs[i,:,:] = signal.convolve2d(chan_i[:,:], window, boundary='wrap', mode='same')
-    print(i)
 
-subbed_epochs = slice_epochs - smooth_epochs
-subbed_ts = np.zeros(graddata.shape)
-for i in np.arange(0,slice_epochs.shape[1]):
-    subbed_ts[:,slice_inds[i,:]] = subbed_epochs[:,i,:]
-    
+fft = np.abs(np.fft.fft(subbed_ts[3,50000:subbed_ts.shape[1]-50000]))
+plt.plot(fft)
+
+
+
+
+
 
 
 
