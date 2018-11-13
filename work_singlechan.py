058f0ef0236ee0e8af2d69c78f4fff8ecb7ef6b0
NeuroDebian User <brain@neurodebian>
2018-11-13 17:53:23 -0500

    Add fitting for single gradient pulses

---
+++ work_singlechan.py
@@ -0,0 +1,163 @@
+import mne as mne
+import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+def get_slicepochs(single_channel, slicegap):
+    print("epoching slices...")
+    nepochs = np.int(single_channel.shape[0] / slicegap)
+    slice_epochs = np.zeros([nepochs, slicegap])
+    slice_inds = np.zeros([nepochs,slicegap])
+    icount = 0
+    for i in np.arange(0, single_channel.shape[0]-slicegap, slicegap):
+        slice_epochs[icount,:] = single_channel[i:i+slicegap]
+        slice_inds[icount,:] = np.arange(i,i+slicegap) 
+        icount = icount + 1 
+            
+    slice_inds = slice_inds.astype(int)
+    return slice_epochs, slice_inds
+
+def find_bad_slices(slice_epochs, corrthresh=0.9):
+    print("finding bad slices (slices without gradient artifacts)")
+    corrmat = np.corrcoef(slice_epochs)
+    mean_corrmat = np.mean(corrmat,axis=1)
+    
+    good_epoch_inds = np.where(mean_corrmat > corrthresh)[0]
+    bad_epoch_inds = np.where(mean_corrmat <= corrthresh)[0]
+    
+    return good_epoch_inds, bad_epoch_inds, mean_corrmat > corrthresh
+
+def replace_bad_slices(slice_epochs, good_epoch_inds, bad_epoch_inds):
+    for i in bad_epoch_inds:
+        dists_i = np.abs(i - good_epoch_inds)
+        min_index = good_epoch_inds[np.argmin(dists_i)]
+        slice_epochs[i,:] = slice_epochs[min_index,:]
+
+    return slice_epochs
+
+def subtract_gradient(slice_epochs, slice_inds, corrmat_thresh, data_l, window_sz=30, window_std=4):
+    window = signal.gaussian(window_sz, std=window_std)
+    window = np.reshape(window / np.sum(window),[window.shape[0],1])   
+    smooth_epochs = np.zeros(slice_epochs.shape)        
+    smooth_epochs[:,:] = signal.convolve2d(slice_epochs, window, boundary='wrap', mode='same')        
+    subbed_epochs = slice_epochs - smooth_epochs
+    
+    subbed_ts = np.zeros(data_l)
+    for i in np.arange(0,slice_epochs.shape[0]):
+        if corrmat_thresh[i] == 1:
+            subbed_ts[slice_inds[i,:]] = subbed_epochs[i,:]
+        else:
+            subbed_ts[slice_inds[i,:]] = 0
+            
+    return subbed_ts
+
+def isolate_frequencies(data,midfreq,fs):
+    spec1 = np.fft.fft(data) 
+    spec2 = np.zeros(spec1.shape,dtype=np.complex_)
+    spec2[0:] = spec1[0:] ; 
+    
+    length = data.shape[0]
+    binsz = fs/length 
+    lowCutIndex = int(midfreq/binsz)
+    spec1[1:lowCutIndex] = 0 
+    spec1[length-lowCutIndex:] = 0 
+    spec2[lowCutIndex+1 : length-lowCutIndex-1] = 0 
+
+    highpass = np.real(np.fft.ifft(spec1))
+    lowpass = np.real(np.fft.ifft(spec2))
+    
+    return highpass,lowpass 
+
+def epoch_timeseries(timeseries, window_length):
+    epochs = np.zeros([int(timeseries.shape[0]/window_length), window_length])
+    icount = 0
+    for i in np.arange(0, timeseries.shape[0]-window_length, window_length):
+        epochs[icount,:] = timeseries[i:i+window_length]
+        icount = icount + 1 
+        
+    return epochs
+
+dat0 = np.load('/media/sf_shared/graddata/graddata_0.npy')
+
+
+hp, lp = isolate_frequencies(dat0,2,5000)
+
+wsize=15000
+single_channel = hp 
+mcorrs = np.zeros([wsize,np.int(single_channel.shape[0]/wsize)])
+icount = 0
+for i in np.arange(0, single_channel.shape[0] - wsize - 1, wsize):
+    mcorrs[:,icount] = signal.correlate(single_channel[i:i+wsize],single_channel[i:i+wsize], mode='same')
+    icount = icount + 1
+    
+mcorrs = np.mean(mcorrs,axis=1)
+slice_gap = np.argmax(mcorrs[np.int(wsize/2)+50:]) + 50
+slice_epochs, slice_inds = get_slicepochs(hp, slice_gap)
+
+peak_grad = np.argmax(np.diff(np.mean(slice_epochs,axis=0)))
+shift_ts = slice_epochs[:,peak_grad]
+shift_epoch = epoch_timeseries(shift_ts, slice_gap*5)
+shift_xcorrs = np.zeros(shift_epoch.shape)
+for i in np.arange(0,shift_epoch.shape[0]):
+    shift_xcorrs[i,:] = signal.correlate(shift_epoch[i,:],shift_epoch[i,:],mode='same')
+
+mean_xcorrs = np.mean(shift_xcorrs,axis=0)
+
+maxinds = np.where(np.r_[True, mean_xcorrs[1:] > mean_xcorrs[:-1]] 
+        & np.r_[mean_xcorrs[:-1] > mean_xcorrs[1:], True])
+
+offset = int(np.median(np.diff(maxinds)))
+
+epochs_i = slice_epochs[0::40,:]
+sort_diffs = np.zeros([epochs_i.shape[0], epochs_i.shape[0]])
+for i in np.arange(0,epochs_i.shape[0]):
+    diffs_i = (np.tile(epochs_i[[i],:],(epochs_i.shape[0],1)) - epochs_i)
+    sumdiffs = np.sum(np.abs(diffs_i),axis=1)
+    sort_diffs[i,:] = np.argsort(sumdiffs)
+    print(i)
+    
+def fit_func(x, a, b):
+    return a*x + b
+    
+    
+sort_diffs = sort_diffs.astype(int)
+sub_epochs = np.zeros(epochs_i.shape)
+for i in np.arange(0, sort_diffs.shape[0]):
+    template_i = np.mean(epochs_i[sort_diffs[i,1:30],:],axis=0)
+    params = curve_fit(fit_func, template_i, epochs_i[i,:])
+    fit_artifact = template_i*params[0][0] + params[0][1]
+    sub_epochs[i,:] = epochs_i[i,:] - fit_artifact
+
+
+
+
+
+"""
+# get all shifted epochs, get their lag, and then shift
+mean_grads = np.zeros([offset, slice_epochs.shape[1]])
+for i in np.arange(0,offset):
+    shift_epochs = slice_epochs[i::40,:]
+    mean_grads[i,:] = np.mean(shift_epochs,axis=0)
+
+res_grads = signal.resample(mean_grads, mean_grads.shape[1]*100, axis=1)
+shift_grads = np.zeros(res_grads.shape)
+lags = np.zeros(offset)
+for i in np.arange(0,offset):
+    xcorr = signal.correlate(res_grads[i,:],res_grads[0,:],mode='same')
+    lags[i] = np.argmax(xcorr) - int(res_grads.shape[1]/2)
+    shift_grads[i,:] = np.roll(res_grads[i,:],-int(lags[i]))
+"""
+# average the up-sampled shifter artifacts, then shift back and down-sample
+
+
+"""
+shift_slice_epochs = np.zeros([slice_epochs.shape[0],slice_epochs.shape[1]-offset])
+for i in np.arange(0,slice_epochs.shape[0]):
+    offset_i = offset-i%offset
+    shift_slice_epochs[i,:] = slice_epochs[i,offset_i:slice_epochs.shape[1]-(offset-offset_i)]
+"""        
+        
+
+
+
