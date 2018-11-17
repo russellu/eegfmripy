d6d5041d9de8a98764891d1cab7ba1ed8d410e39
gmierz <gmierz1@live.ca>
2018-11-17 12:26:06 -0500

    Add pytest structuring and prepare for Travis.

---
+++ eegfmripy/tests/TestsEEGFMRI.py
@@ -0,0 +1,16 @@
+import mne as mne
+
+"""
+Test code and example data for eegfmripy. 
+
+"""
+
+def test_example_raw():
+    montage = mne.channels.read_montage('standard-10-5-cap385', path='/media/sf_shared/')
+    raw = mne.io.read_raw_brainvision(
+        '/media/sf_shared/CoRe_011/eeg/CoRe_011_Day2_Night_01.vhdr',
+        montage=montage,
+        eog=['ECG','ECG1']
+    )
+    
+    return raw
