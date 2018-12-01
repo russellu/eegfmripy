491f76112ff2aeab9d5022cff5247b18bdf2f8dc
russellu <russell.buttler@usherbrooke.ca>
2018-12-01 11:57:27 -0500

    Add source localization examples from mne website

---
+++ eegfmripy/setup_src_space.py
@@ -0,0 +1,19 @@
+import os.path as op
+
+import numpy as np
+from mayavi import mlab
+
+import mne
+from mne.datasets import sample
+
+print(__doc__)
+
+data_path = sample.data_path()
+subjects_dir = op.join(data_path, 'subjects')
+raw_fname = op.join(data_path, 'MEG', 'sample', 'sample_audvis_raw.fif')
+trans_fname = op.join(data_path, 'MEG', 'sample',
+                      'sample_audvis_raw-trans.fif')
+raw = mne.io.read_raw_fif(raw_fname)
+trans = mne.read_trans(trans_fname)
+src = mne.read_source_spaces(op.join(subjects_dir, 'sample', 'bem',
+                                     'sample-oct-6-src.fif'))
---
+++ eegfmripy/test_srcloc.py
@@ -0,0 +1,74 @@
+# sphinx_gallery_thumbnail_number = 10
+import numpy as np
+import matplotlib.pyplot as plt
+import mne
+from mne.datasets import sample
+from mne.minimum_norm import make_inverse_operator, apply_inverse
+
+data_path = sample.data_path()
+raw_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw.fif'
+
+raw = mne.io.read_raw_fif(raw_fname)  # already has an average reference
+events = mne.find_events(raw, stim_channel='STI 014')
+
+event_id = dict(aud_l=1)  # event trigger and conditions
+tmin = -0.2  # start of each epoch (200ms before the trigger)
+tmax = 0.5  # end of each epoch (500ms after the trigger)
+raw.info['bads'] = ['MEG 2443', 'EEG 053']
+picks = mne.pick_types(raw.info, meg=True, eeg=False, eog=True,
+                       exclude='bads')
+baseline = (None, 0)  # means from the first instant to t = 0
+reject = dict(grad=4000e-13, mag=4e-12, eog=150e-6)
+
+epochs = mne.Epochs(raw, events, event_id, tmin, tmax, proj=True, picks=picks,
+                    baseline=baseline, reject=reject)
+
+noise_cov = mne.compute_covariance(
+    epochs, tmax=0., method=['shrunk', 'empirical'], verbose=True)
+
+fig_cov, fig_spectra = mne.viz.plot_cov(noise_cov, raw.info)
+
+evoked = epochs.average().pick_types(meg=True)
+evoked.plot(time_unit='s')
+evoked.plot_topomap(times=np.linspace(0.05, 0.15, 5), ch_type='mag',
+                    time_unit='s')
+
+# Show whitening
+evoked.plot_white(noise_cov, time_unit='s')
+
+del epochs  # to save memory
+
+# Read the forward solution and compute the inverse operator
+fname_fwd = data_path + '/MEG/sample/sample_audvis-meg-oct-6-fwd.fif'
+fwd = mne.read_forward_solution(fname_fwd)
+
+# make an MEG inverse operator
+info = evoked.info
+inverse_operator = make_inverse_operator(info, fwd, noise_cov,
+                                         loose=0.2, depth=0.8)
+del fwd
+
+# You can write it to disk with::
+#
+#     >>> from mne.minimum_norm import write_inverse_operator
+#     >>> write_inverse_operator('sample_audvis-meg-oct-6-inv.fif',
+#                                inverse_operator)
+
+method = "dSPM"
+snr = 3.
+lambda2 = 1. / snr ** 2
+stc, residual = apply_inverse(evoked, inverse_operator, lambda2,
+                              method=method, pick_ori=None,
+                               verbose=True)
+
+
+
+
+
+
+
+
+
+
+
+
