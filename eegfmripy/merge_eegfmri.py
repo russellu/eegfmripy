62ae874cf073553547cbb98b16fc1c34841e77f0
russellu <russell.buttler@usherbrooke.ca>
2018-12-02 15:10:43 -0500

    Add alignment of epoching of BOLD and EEG

--- eegfmripy/merge_eegfmri.py
+++ eegfmripy/merge_eegfmri.py
@@ -13,83 +13,6 @@ functions to merge ICA denoised EEG data with BOLD ICA components
 
 """
 
-
-"""
-epoch BOLD components based on triggers and compute power spectral density
-for each trigger type and average across all epochs
-
-returns an array of [trigger, component, power density] and the
-frequency axis of the power density 
-
-"""
-def pxx_bold_component_epoch(bold_comps, bold_srate, 
-                             eeg_srate, bold_epochl, triggers): 
-    
-    bold_pxx = np.zeros((4,bold_comps.shape[1],7))
-    stage_count = 0 
-    for stage in triggers:
-        print(stage)
-        stage_inds = [index for index, value in enumerate(event_ids) if value == stage]
-        stage_lats = event_lats[stage_inds] - start_ind 
-        neg_inds = np.where(stage_lats < 0)
-        stage_lats = np.delete(stage_lats,neg_inds)
-        tr_lats = stage_lats / (eeg_srate/bold_srate)
-        tr_lats = tr_lats.astype(int)
-        
-        lat_epochs = np.zeros((bold_comps.shape[1], tr_lats.shape[0],int(bold_epochl)))
-        for lat in np.arange(0,tr_lats.shape[0]):
-            delete_last = False
-            if tr_lats[lat] + bold_epochl < bold_comps.shape[0]:
-                lat_epochs[:,lat,:] = bold_comps[tr_lats[lat]:tr_lats[lat]+bold_epochl,:].transpose()
-            else:
-                delete_last = True
-            
-            if delete_last == True:
-                lat_epochs = np.delete(lat_epochs,lat_epochs.shape[1]-1,axis=1)
-    
-        f,pxx = signal.welch(lat_epochs,bold_srate)
-        bold_pxx[stage_count,:,:] = np.mean(pxx,axis=1)
-        stage_count = stage_count + 1
-
-    return bold_pxx, f
-
-"""
-pxx_eeg_component_epochs
-epoch EEG signal based on triggers, and compute average power spectral 
-density for each trigger
-
-returns array of [trigger, electrode, frequency] and the frequency axis
-
-"""
-def pxx_eeg_epochs(raw_data, triggers, epoch_samples ):
-    
-    stage_power = np.zeros((len(triggers),raw_data.shape[0],129))
-    stage_count = 0 
-    for stage in triggers:
-        print(stage)
-        stage_inds = [index for index, value in enumerate(event_ids) if value == stage]
-        stage_lats = event_lats[stage_inds]
-        lat_epochs = np.zeros((raw_data.shape[0],stage_lats.shape[0],epoch_samples))
-        for lat in np.arange(0,lat_epochs.shape[1]):
-            delete_last = False 
-            if stage_lats[lat] + epoch_samples < raw_data.shape[1]:
-                lat_epochs[:,lat,:] = raw_data[:, stage_lats[lat]:stage_lats[lat]+epoch_samples]        
-            else:
-                delete_last = True
-         
-        if delete_last == True:
-            np.delete(lat_epochs, lat_epochs.shape[1] - 1, axis=1)
-                       
-        pxx = np.zeros((raw_data.shape[0],lat_epochs.shape[1],129))
-        for chan in np.arange(0,raw_data.shape[0]):
-            freqs, pxx[chan,:,:] = signal.welch(lat_epochs[chan,:,:],fs=250)
-        
-        stage_power[stage_count,:,:] = np.mean(pxx,axis=1)
-        stage_count = stage_count + 1
-    
-    return stage_power, freqs
-
-
 eeg_path = '/media/sf_shared/graddata/ica_denoised_raw.fif' 
 fmri_path = '/media/sf_shared/CoRe_011/rfMRI/d2/11-BOLD_Sleep_BOLD_Sleep_20150824220820_11.nii'
 vmrk_path = '/media/sf_shared/CoRe_011/eeg/CoRe_011_Day2_Night_01.vmrk'
@@ -123,7 +46,7 @@ raw = mne.io.read_raw_fif(eeg_path,preload=True)
 raw_data = raw.get_data()
     
 # get power spectrum for different sleep stages (BOLD)
-comps = canica.transform([fmri])[0]
+comps = canica.transform([fmri])[0].transpose()
 bold_srate = 1/fmri_info[0]
 bold_epochl = int(7500 / (250/bold_srate))
 
@@ -138,34 +61,39 @@ def get_trigger_inds(trigger_name, event_ids):
     return trig_inds
 
 def epoch_triggers(raw_data, lats, pre_samples, post_samples):
-    epochs = np.zeros((raw_data.shape[0], trig_lats.shape[0], pre_samples + post_samples))
-    delete_last = False
-    for lat in np.arange(0,trig_lats.shape[0]):
-        if (trig_lats[lat]-pre_samples > 0) & (
-        trig_lats[lat] + post_samples < raw_data.shape[1]):
-            epochs[:,lat,:] = raw_data[:,trig_lats[lat]-pre_samples : 
-                                         trig_lats[lat]+post_samples] 
-        else:
-            delete_last = True
-            
-    if delete_last == True:
-        epochs = np.delete(epochs,epochs.shape[1]-1, axis=1)
-    
+    epochs = np.zeros((raw_data.shape[0], lats.shape[0], pre_samples + post_samples))
+    for lat in np.arange(0,lats.shape[0]):
+        epochs[:,lat,:] = raw_data[:,lats[lat]-pre_samples : lats[lat]+post_samples] 
+
     return epochs
 
-pre_samples = 0 
-post_samples = 7500
-srate = 250 
 trigger_names = ['wake','NREM1','NREM2','NREM3']
 
-all_pxx = np.zeros([4,63,129])
+"""
+epoch BOLD and get power for different trigger types 
+what you actually want is single trial EEG and BOLD psd
+
+first get all the indices that are contained within the BOLD timeseries
+then, get the EEG power spectrum values within those same indices 
+"""
+bold_pre_samples = 5
+bold_post_samples = 15 
+eeg_pre_samples = int(bold_pre_samples*fmri_info[0]*eeg_srate)
+eeg_post_samples = int(bold_post_samples*fmri_info[0]*eeg_srate)
+bold_conversion = eeg_srate / (1/fmri_info[0])
+
+all_bold_epochs = []
 for trig in np.arange(0, len(trigger_names)):
     trig_inds = get_trigger_inds(trigger_names[trig], event_ids)
     trig_lats = event_lats[trig_inds]
-    epochs = epoch_triggers(raw_data, trig_lats, pre_samples, post_samples)
-    epochs = np.reshape(epochs,[epochs.shape[0],epochs.shape[1]*epochs.shape[2]])
-    f, pxx = signal.welch(epochs, srate)
-    all_pxx[trig,:,:] = pxx
-# have another function for computing the coupling during different epochs
+    bold_lats = ((trig_lats - start_ind) / bold_conversion).astype(int)    
+    bads = np.where((bold_lats-bold_pre_samples<0) | (bold_lats+bold_post_samples>=comps.shape[1]))
+    
+    bold_lats = np.delete(bold_lats,bads,axis=0)
+    eeg_lats = np.delete(trig_lats,bads,axis=0)
+    
+    bold_epochs = epoch_triggers(comps, bold_lats, bold_pre_samples, bold_post_samples)
+    eeg_epochs = epoch_triggers(raw_data,eeg_lats,eeg_pre_samples, eeg_post_samples)
 
+    all_bold_epochs.append(bold_epochs)
 
