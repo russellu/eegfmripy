e9e949420f533f0dc9a169985c8ed14e683dd70c
russellu <russell.buttler@usherbrooke.ca>
2018-12-03 00:21:39 -0500

    Add eeg downsampling and convolution, Add cross-correlation function

--- eegfmripy/merge_eegfmri.py
+++ eegfmripy/merge_eegfmri.py
@@ -9,10 +9,58 @@ import sys as sys
 sys.path.insert(0, '/media/sf_shared/eegfmripy/eegfmripy')
 import helpers
 from scipy import stats
+from scipy.stats import gamma
+
 """
 functions to merge ICA denoised EEG data with BOLD ICA components
 
 """
+from scipy.signal import butter, lfilter
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+def get_hrf(times):
+    peak_values = gamma.pdf(times, 6)
+    undershoot_values = gamma.pdf(times, 12)
+    values = peak_values - 0.35 * undershoot_values
+    return values / np.max(values) * 0.6
+
+def filter_and_downsample(raw_data, comps, freqs, start_ind, end_ind):
+    chan_freqs = np.zeros((raw_data.shape[0], freqs.shape[0], comps.shape[1]))
+    for i in np.arange(0,freqs.shape[0]):
+        filt = butter_bandpass_filter(raw_data, freqs[i,0],freqs[i,1], 250)
+        filt = np.abs(filt)
+        gauss_filt = np.zeros(filt.shape)
+        for j in np.arange(0,raw_data.shape[0]):
+            smooth_filt = signal.convolve(filt[j,:],gauss,mode='same')
+            gauss_filt[j,:] = smooth_filt[0:raw_data.shape[1]]
+        
+        gauss_filt = transform.resize(
+                gauss_filt[:,start_ind:end_ind],[filt.shape[0],comps.shape[1]])
+        
+        chan_freqs[:,i,:] = gauss_filt
+        print(i)
+    
+    return chan_freqs
+
+def convolve_chanfreqs(chan_freqs, hrf):
+    conved = np.zeros(chan_freqs.shape)
+    for i in np.arange(0,chan_freqs.shape[0]):
+        for j in np.arange(0,chan_freqs.shape[1]):
+            conved_ij = signal.convolve(chan_freqs[i,j,:], hrf, mode='full')
+            conved[i,j,:] = conved_ij[0:chan_freqs.shape[2]]
+            
+    return conved
 
 eeg_path = '/media/sf_shared/graddata/ica_denoised_raw.fif' 
 fmri_path = '/media/sf_shared/CoRe_011/rfMRI/d2/11-BOLD_Sleep_BOLD_Sleep_20150824220820_11.nii'
@@ -27,7 +75,6 @@ grad_lats = grad_lats / 20 # resample from 5000Hz to 250Hz
 start_ind = int(grad_lats[0])
 end_ind = int(grad_lats[-1])
 
-
 canica = CanICA(n_components=40, smoothing_fwhm=6.,
                 threshold=None, verbose=10, random_state=0)
 
@@ -36,6 +83,11 @@ fmri = nib.load(fmri_path)
 fmri_info = helpers.fmri_info(fmri_path)
 canica.fit(fmri)
 cimg = canica.components_img_.get_data()
+TR = fmri_info[0]
+tr_times = np.arange(0, 30, TR)
+hrf = get_hrf(tr_times)
+
+
 
 # plot components 
 for i in np.arange(0,40):
@@ -117,19 +169,7 @@ for i in np.arange(0,bold_pxx.shape[0]):
             r[i,j,k],p = stats.pearsonr(bold_pxx[i,:,j],eeg_pxx[0,:,k])
             
 
-from scipy.signal import butter, lfilter
 
-def butter_bandpass(lowcut, highcut, fs, order=5):
-    nyq = 0.5 * fs
-    low = lowcut / nyq
-    high = highcut / nyq
-    b, a = butter(order, [low, high], btype='band')
-    return b, a
-
-def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
-    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
-    y = lfilter(b, a, data)
-    return y
 
 #skimage.transform.resize(image, 
 #                         output_shape, order=1, mode='reflect', cval=0, 
@@ -143,25 +183,66 @@ gauss = signal.gaussian(eeg_srate, 20)
 gauss = gauss/np.sum(gauss)
 
 
-freqs = np.arange(2,122,2)
-chan_freqs = np.zeros((raw_data.shape[0], freqs.shape[0], comps.shape[1]))
-for i in np.arange(0,freqs.shape[0]):
-    filt = butter_bandpass_filter(raw_data, freqs[i]-1,freqs[i]+1, 250)
-    filt = np.abs(filt)
-    gauss_filt = np.zeros(filt.shape)
-    for j in np.arange(0,raw_data.shape[0]):
-        smooth_filt = signal.convolve(filt[j,:],gauss,mode='same')
-        gauss_filt[j,:] = smooth_filt[0:raw_data.shape[1]]
+freqs = np.zeros((5,2))
+freqs[0,0] = 1; freqs[0,1] = 3
+freqs[1,0] = 4; freqs[1,1] = 7
+freqs[2,0] = 8; freqs[2,1] = 15
+freqs[3,0] = 17; freqs[3,1] = 30
+freqs[4,0] = 30; freqs[4,1] = 80
+
+chan_freqs = filter_and_downsample(raw_data, comps, freqs, start_ind, end_ind)
+conved = convolve_chanfreqs(np.log(chan_freqs), hrf)
+
+
+
+# epoch all the hrf-convolved filtered EEG power 
+all_conved_epochs = []
+for trig in np.arange(0, len(trigger_names)):
+    trig_inds = get_trigger_inds(trigger_names[trig], event_ids)
+    trig_lats = event_lats[trig_inds]
+    bold_lats = ((trig_lats - start_ind) / bold_conversion).astype(int)    
+    bads = np.where((bold_lats-bold_pre_samples<0) | (bold_lats+bold_post_samples>=comps.shape[1]))    
+    bold_lats = np.delete(bold_lats,bads,axis=0)
     
-    gauss_filt = transform.resize(
-            gauss_filt[:,start_ind:end_ind],[filt.shape[0],comps.shape[1]])
+    conved_epochs = np.zeros(
+            (conved.shape[0],conved.shape[1],bold_lats.shape[0],bold_pre_samples+bold_post_samples))
+    for i in np.arange(0, conved.shape[1]):
+        conved_epochs[:,i,:] = epoch_triggers(
+                conved[:,i,:], bold_lats, bold_pre_samples, bold_post_samples)
+
+    all_conved_epochs.append(conved_epochs)
+
+
+all_xcorrs = np.zeros((40,50))
+for c in np.arange(0,40):
+    sig1 = chan_freqs[3,2,:]
+    sig2 = comps[c,:]
+    sig2 = butter_bandpass_filter(sig2,0.005,0.1,1/fmri_info[0])
+    nlags = 50
     
-    chan_freqs[:,i,:] = gauss_filt
-    print(i)
+    vec_l = sig1.shape[0] - nlags
+    xcorrs = np.zeros(nlags)
+    vec1 = sig1[int(sig1.shape[0]/2 - vec_l/2) : int(sig1.shape[0]/2+ vec_l/2)]
+    start_p = 0
+    for i in np.arange(0,nlags):
+        vec2 = sig2[(start_p+i):(start_p+vec_l+i)]    
+        xcorrs[i] = np.corrcoef(vec1,vec2)[0,1]
+        
+    all_xcorrs[c,:] = xcorrs 
+        
 
+    
+"""
+current_corrs = np.zeros((4, all_conved_epochs[0].shape[0],
+                all_conved_epochs[0].shape[1], all_bold_epochs[0].shape[0]))
+for i in np.arange(0,len(all_conved_epochs)):   
 
-
-
-
-
+    for j in np.arange(0,all_conved_epochs[i].shape[0]):
+        print(j)
+        for k in np.arange(0,all_conved_epochs[i].shape[1]):
+            for el in np.arange(0,all_bold_epochs[i].shape[0]):
+                    r,p = stats.pearsonr(all_conved_epochs[i][j,k,:,:].flatten(),
+                                         all_bold_epochs[i][el,:,:].flatten())                   
+                    current_corrs[i,j,k,el] = r
+"""
 
